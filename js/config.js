8c130f5cd3e46590d803c42ceb1f00c759f709b0
KingKCLee <noble.kclee@gmail.com>
2026-03-11 18:05:37 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@
 // =============================================
 
 const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY_HERE';
+const SUPABASE_ANON_KEY = 'sb_publishable_0gB-14Z0t5ewxQ3J_epBbQ_ZCYMFoAc';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
