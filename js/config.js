60bfcfd8bbc17b33600b1c14cb44fbe37a978041
KingKCLee <noble.kclee@gmail.com>
2026-03-11 19:35:16 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // 아래 값을 본인의 Supabase 프로젝트 값으로 교체하세요
 // =============================================
 
-const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_0gB-14Z0t5ewxQ3J_epBbQ_ZCYMFoAc';
+const SUPABASE_URL = 'https://srlkttykoqbmrusbavzi.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_0gB-1420t5ewxQ3J_epBbQ_ZCYMFoAc';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
